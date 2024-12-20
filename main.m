bee94a94ca86db70ef857a350c8177de5911e01e
lleoo0110 <rh130110@gmail.com>
2024-12-20 14:44:25 +0900

    getConfigパラメータ修正

--- main.m
+++ main.m
@@ -23,7 +23,7 @@ setupPaths(currentDir);
 
 
 %%  指定のパラメータで実行
-params = getConfig('epocx', 'preset', 'ahaloud');
+params = getConfig('epocx', 'preset', 'ddaExperiment');
 manager = EEGAcquisitionManager(params);        % データ計測
 
 
